1e374f8d652f213e085c9e07e101cd83aa9ede93
Darryl Gouder <dargouder@gmail.com>
2019-04-02 11:35:42 +0100

    fixed sphere scene.

--- source/main.cpp
+++ source/main.cpp
@@ -18,7 +18,7 @@
 #include <fstream>
 #include <iostream>
 
-ONB shadingONB;
+static ONB shadingONB;
 Vec3 colourNormal(const Ray &ray, const Hitable &world, int depth) {
   HitRecord rec;
   if (world.hit(ray, 0.001, FLT_MAX, rec)) {
@@ -79,146 +79,14 @@ Vec3 colourRecursive(const Ray &ray, const Hitable &world, int depth) {
   }
 }
 
-// Vec3 colourRecursive(const Ray &ray, const Hitable &world, int depth) {
-//  HitRecord rec;
-//  if (world.hit(ray, 0.001, FLT_MAX, rec)) {
-//    Ray scattered;
-//    Vec3 attenuation;
-//    float u = 0.0f, v = 0.0f;
-//    rec.normal.make_unit_vector();
-//    Vec3 emitted = rec.mat_ptr->Le(0, 0, rec.p);
-//    float pdf;
-//    if (depth < 10 &&
-//        rec.mat_ptr->sample_f(ray, rec, attenuation, scattered, pdf)) {
-//      Vec3 f = attenuation / pdf;
-//      // assert(pdf > 0);
-//      float cos_theta = Vec3::dot(rec.normal, scattered.direction());
-//      return emitted +
-//             f * cos_theta * colourRecursive(scattered, world, depth + 1);
-//    } else {
-//      return emitted;
-//    }
-//  } else {
-//    Vec3 unit_direction = Vec3::unit_vector(ray.direction());
-//    float t = 0.5f * (unit_direction.y + 1.0f);
-//    Vec3 col = (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0);
-//    return col;
-//  }
-//}
-
-// void CornellBox(HitableList &list, Hitable **light) {
-//  // floor
-//  list.list.push_back(std::make_unique<Quad>(
-//      Vec3(-10.0f, 0.0f, -10.0f), Vec3(-10.0f, 0.0f, 10.0f),
-//      Vec3(10.0f, 0.0f, 10.0f), Vec3(10.0f, 0.0f, -10.0f),
-//      std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-//  //list.list.push_back(std::make_unique<Sphere>(
-//  //    Vec3(0.0f, 0.5f, 0.5f), 0.5f,
-//  //    std::make_unique<LambertianReflection>(Vec3(0.0f, 0.9f, 0.0f))));
-//
-//  //list.list.push_back(std::make_unique<Quad>(
-//  //    Vec3(-0.5f, 1.0f, -0.5f), Vec3(-0.5f, 1.0f, 0.5f),
-//  //    Vec3(0.5f, 1.0f, 0.5f), Vec3(0.5f, 1.0f, -0.5f),
-//  //    std::make_unique<LambertianReflection>(Vec3(1.0f, 1.0f, 0.0f))));
-//
-//  list.list.push_back(std::make_unique<Quad>(
-//      Vec3(-0.5f, 1.0f, -0.5f), Vec3(-0.5f, 1.0f, 0.5f),
-//      Vec3(0.5f, 1.0f, 0.5f), Vec3(0.5f, 1.0f, -0.5f),
-//      std::make_unique<AreaLight>(Vec3(1.0f, 1.0f, 1.0f))));
-//
-//  // list.list.push_back(std::make_unique<Sphere>(
-//  //    Vec3(0.0f, 0.5f, 0.5f), 0.5f,
-//  //    std::make_unique<Phong>(Vec3(0.4f, 0.4f, 0.4f), Vec3(0.05f, 0.05f,
-//  //    0.05f))));
-//}
 
 void CornellBox(HitableList &list, Hitable *light) {
-  //// floor
-  // list.list.push_back(std::make_unique<Quad>(
-  //    Vec3(552.8f, 0.0f, 0.0f), Vec3(0.0f, 0.0f, 0.0f),
-  //    Vec3(0.0f, 0.0f, 559.2f), Vec3(549.6f, 0.0f, 559.2f),
-  //    std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-
-  //// ceiling
-  // list.list.push_back(std::make_unique<Quad>(
-  //    Vec3(556.0f, 548.8f, 0.0f), Vec3(556.0f, 548.8f, 559.2f),
-  //    Vec3(0.0f, 548.8f, 559.2f), Vec3(0.0f, 548.8f, 0.0f),
-  //    std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-
-  //// back wall
-  // list.list.push_back(std::make_unique<Quad>(
-  //    Vec3(549.6f, 0.0f, 559.2f), Vec3(0.0f, 0.0f, 559.2f),
-  //    Vec3(0.0f, 548.8f, 559.2f), Vec3(549.6f, 548.8f, 559.2f),
-  //    std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
 
   // Sphere
   list.list.push_back(std::make_unique<Sphere>(
-      Vec3(0, 0, -2), 1,
+      Vec3(0, 0, 0), 1,
       std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
 
-  //// right wall
-  // list.list.push_back(std::make_unique<Quad>(
-  //    Vec3(0.0f, 0.0f, 559.2f), Vec3(0.0f, 0.0f, 0.0f),
-  //    Vec3(0.0f, 548.8f, 0.0f), Vec3(0.0f, 548.8f, 559.2f),
-  //    std::make_unique<LambertianReflection>(Vec3(0.14f, 0.45f, 0.091f))));
-
-  //// left wall
-  // list.list.push_back(std::make_unique<Quad>(
-  //    Vec3(549.6f, 0.0f, 0.0f), Vec3(549.6f, 0.0f, 559.2f),
-  //    Vec3(549.6f, 548.8f, 559.2f), Vec3(549.6f, 548.8f, 0.0f),
-  //    std::make_unique<LambertianReflection>(Vec3(0.63f, 0.065f, 0.05f))));
-
-  // short block
-  // list.list.push_back(std::make_unique<Quad>(
-  //  Vec3(130.0, 165.0, 65.0), Vec3(82.0, 165.0, 225.0),
-  //  Vec3(240.0, 165.0, 272.0), Vec3(290.0, 165.0, 114.0),
-  //  std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-  // list.list.push_back(std::make_unique<Quad>(
-  //  Vec3(290.0, 0.0, 114.0), Vec3(290.0, 165.0, 114.0),
-  //  Vec3(240.0, 165.0, 272.0), Vec3(240.0, 0.0, 272.0),
-  //  std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-  // list.list.push_back(std::make_unique<Quad>(
-  //  Vec3(130.0, 0.0, 65.0), Vec3(130.0, 165.0, 65.0),
-  //  Vec3(290.0, 165.0, 114.0), Vec3(290.0, 0.0, 114.0),
-  //  std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-  // list.list.push_back(std::make_unique<Quad>(
-  //  Vec3(82.0, 0.0, 225.0), Vec3(82.0, 165.0, 225.0),
-  //  Vec3(130.0, 165.0, 65.0), Vec3(130.0, 0.0, 65.0),
-  //  std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-  // list.list.push_back(std::make_unique<Quad>(
-  //  Vec3(240.0, 0.0, 272.0), Vec3(240.0, 165.0, 272.0),
-  //  Vec3(82.0, 165.0, 225.0), Vec3(82.0, 0.0, 225.0),
-  //  std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-  // long block
-  /* list.list.push_back(std::make_unique<Quad>(
-     Vec3(423.0, 330.0, 247.0), Vec3(265.0, 330.0, 296.0),
-     Vec3(314.0, 330.0, 456.0), Vec3(472.0, 330.0, 406.0),
-     std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-   list.list.push_back(std::make_unique<Quad>(
-     Vec3(423.0, 0.0, 247.0), Vec3(423.0, 330.0, 247.0),
-     Vec3(472.0, 330.0, 406.0), Vec3(472.0, 0.0, 406.0),
-     std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-   list.list.push_back(std::make_unique<Quad>(
-     Vec3(472.0, 0.0, 406.0), Vec3(472.0, 330.0, 406.0),
-     Vec3(314.0, 330.0, 456.0), Vec3(314.0, 0.0, 456.0),
-     std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-   list.list.push_back(std::make_unique<Quad>(
-     Vec3(314.0, 0.0, 456.0), Vec3(314.0, 330.0, 456.0),
-     Vec3(265.0, 330.0, 296.0), Vec3(265.0, 0.0, 296.0),
-     std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));
-   ;
-   list.list.push_back(std::make_unique<Quad>(
-     Vec3(265.0, 0.0, 296.0), Vec3(265.0, 330.0, 296.0),
-     Vec3(423.0, 330.0, 247.0), Vec3(423.0, 0.0, 247.0),
-     std::make_unique<LambertianReflection>(Vec3(0.725f, 0.71f, 0.68f))));*/
-
-  // light
-  // list.list.push_back(std::make_unique<Quad>(
-  // Vec3(343.0f, 548.8f, 227.0f), Vec3(343.0f, 548.8f, 332.0f),
-  // Vec3(213.0f, 548.8f, 332.2f), Vec3(213.0f, 548.8f, 227.0f),
-  // std::make_unique<AreaLight>(Vec3(17.0f, 12.0f, 4.0f))));
-
-  // light = list.list[list.list.size() - 1].get();
 }
 
 //#define SINGLE_RAY
@@ -246,7 +114,7 @@ int main() {
 
   light = world.list[world.list.size() - 1].get();
 
-  Vec3 lookfrom(0.0f, 0.0f, -6.0f), lookat(0.0f, 0.0f, -1.0f);
+  Vec3 lookfrom(0.0f, 0.0f, 4.0f), lookat(0.0f, 0.0f, 0.0f);
   //Vec3 lookfrom(278.0f, 292.0f, -800.0f), lookat(278.0f, 292.0f, -799.0f);
   float dist_to_focus = (lookfrom - lookat).length();
   float aperture = 0.0f;
